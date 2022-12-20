39e8405c0263b6229a219767ae18fcc7838b5d66
THRINESH <THRINESH@DESKTOP>
2022-12-20 13:17:15 +0530

    1st commit for jenkins

---
+++ src/test/java/jenkinsPractice/practice.java
@@ -0,0 +1,21 @@
+package jenkinsPractice;
+
+import org.testng.annotations.Test;
+
+public class practice 
+
+{
+
+	@Test
+	public void jenTest1()
+	{
+		System.out.println("1 Test case Jenkins" );
+	}
+	
+	@Test
+	public void jenTest2()
+	{
+		System.out.println("2 Test case Jenkins" );
+	}
+
+}
